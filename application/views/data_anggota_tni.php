c47b2cb69499955ab58cbbe8eebdb51ff9d3c7f7
danupras01 <danupras01@gmail.com>
2019-04-23 09:31:09 +0700

    modifikasi data

--- application/views/data_anggota_tni.php
+++ application/views/data_anggota_tni.php
@@ -11,6 +11,8 @@
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+	<link href="/https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
 
 
 </head>
@@ -22,10 +24,21 @@
 	<a href="data/add" class="btn btn-primary">Tambah Data Anggota TNI</a>
 	<a href="users/index" class="btn btn-primary">Setting User</a>
 
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+	<script type="text/javascript" src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+	<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+
 	<table class="table tabel_anggota" id="">
 		<thead>
 			<tr>
-				<th>#</th>
+				<th>No</th>
 				<th>Nama Anggota</th>
 				<th>Pangkat</th>
 				<th>Korps</th>
@@ -70,8 +83,40 @@
 </div>
 	<script>
 	$(document).ready( function () {
-	$('.tabel_anggota').DataTable();
+	$('.tabel_anggota').DataTable({
+		dom: 'Bfrtip',
+        buttons: [
+        { 
+        	extend:'copy', 
+        },
+        {
+        	extend:'csv'
+        },
+        {
+        	extend:'excel'
+        },
+        // {
+        // 	extend:'pdf'
+        // }
+        { 	
+        	extend: 'pdfHtml5',
+            orientation: 'landscape',
+            pageSize: 'A4',
+            exportOptions: {
+            	modifier: {
+            		page: 'current'
+            	},
+            	columns: [0,1,2,3,4,5,6,7,8,9]            
+            }
+        },
+        {
+        	extend:'print'
+        }
+        ]
+		});
 	} );
+
+
 	</script>	
 </body>
 </html>
